50d031b7ccedc69aa5b40821cc7df2722dc777ae
mayto420 <mcveikka@outlook.com>
2024-01-24 19:13:25 +0200

    fix header not updating

--- game.h
+++ game.h
@@ -1,43 +1,19 @@
 #ifndef GAME_H
 #define GAME_H
 #include <iostream>
-
 class Game {
+private:
     int maxNumber;
     int playerGuess;
     int randomNumber;
     int numOfGuesses;
+    void printGameResult();
+
 public:
-    Game();
-    Game gameObject   () {
-        maxNumber = 10;
-        std::srand(static_cast<unsigned int>(time(0)));
-        randomNumber = std::rand() % maxNumber + 1;
-        numOfGuesses = 0;
-        std::cout << "CONSTRUCTOR Game object created with 10 as max value";
-    }
+    Game(int);
+    ~Game();
+    void play();
 
-
-    ~Game () {
-        std::cout << "DESTRUCTOR Object cleared from stack memory";
-    }
-    void play() {
-        while (true) {
-            std::cout << "Give your guess between 1 and 10 ";
-
-            std::cin >> playerGuess;
-            numOfGuesses++;
-
-            if (playerGuess == randomNumber) {
-                std::cout << "Correct the number was " << randomNumber << ". With " << numOfGuesses << " guesses.\n";
-                break;
-            } else if (playerGuess < randomNumber) {
-                std::cout << "The number is bigger" << std::endl;
-            } else {
-                std::cout << "The number is smaller" << std::endl;
-            }
-        }
-    }
 };
 
 
